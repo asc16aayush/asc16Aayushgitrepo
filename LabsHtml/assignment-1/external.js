9559ef97b84d812048bd217a1bbf2a4c5ff17460
Aayush Raj <aayush.raj@ascendion.com>
2024-10-09 11:02:54 +0530

    completed

---
+++ LabsHtml/assignment-1/external.js
@@ -0,0 +1,12 @@
+console.log("external js");
+function externalscript(){
+    console.log("the external script / modified");
+}
+externalscript();
+
+function loop_demo(){
+for(var i=0;i<5;i++){
+    console.log(i);
+    console.log("value of i is = "+i);
+}
+}
